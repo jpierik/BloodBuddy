393eaed2d06e2946015c8478079a3da3899ac364
SerarMatloob <serarmatloob@oakland.edu>
2016-10-08 00:45:05 -0400

    Design changes
    updated icon

--- app/src/main/java/edu/oakland/fives/bloodbuddy/activity/MainActivity.java
+++ app/src/main/java/edu/oakland/fives/bloodbuddy/activity/MainActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
@@ -44,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+// | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+// | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);;
         //start app intro
         // startAppIntro();
 
